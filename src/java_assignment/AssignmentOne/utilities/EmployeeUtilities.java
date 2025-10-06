eadf4f01055e0d5f8c826d52592958403c9f733f
dhine <dhine@Dk>
2025-10-06 12:38:44 +0530

    commit

--- src/java_assignment/AssignmentOne/utilities/EmployeeUtilities.java
+++ src/java_assignment/AssignmentOne/utilities/EmployeeUtilities.java
@@ -4,7 +4,20 @@ public class EmployeeUtilities {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 private String programmingLanguage;
 
+		    public Developer(String name, int employeeId, double salary, String programmingLanguage) {
+		        super(name, employeeId, salary);
+		        this.programmingLanguage = programmingLanguage;
+		    }
+
+		    public String getProgrammingLanguage() {
+		        return programmingLanguage;
+		    }
+
+		    public void setProgrammingLanguage(String programmingLanguage) {
+		        this.programmingLanguage = programmingLanguage;
+		    }
 	}
 
 }
