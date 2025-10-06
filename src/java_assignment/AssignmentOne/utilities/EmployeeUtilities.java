31f2d868589184d252c46fe7ccddbacfad2d0a00
dhine <dhine@Dk>
2025-10-06 12:40:11 +0530

    commit

--- src/java_assignment/AssignmentOne/utilities/EmployeeUtilities.java
+++ src/java_assignment/AssignmentOne/utilities/EmployeeUtilities.java
@@ -2,8 +2,6 @@ package java_assignment.AssignmentOne.utilities;
 
 public class EmployeeUtilities {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		 private String programmingLanguage;
 
 		    public Developer(String name, int employeeId, double salary, String programmingLanguage) {
